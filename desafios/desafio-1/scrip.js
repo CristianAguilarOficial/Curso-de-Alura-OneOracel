77f4a4da28c4b8cba3a46602e977dd87be8b1bae
CristianAguilarOficial <aguilarcristian060@gmail.com>
2024-06-23 13:54:20 -0500

    ingresamos el desafio

---
+++ desafios/desafio-1/scrip.js
@@ -0,0 +1,13 @@
+//creacion de variables
+let nombre = prompt('Escriba tu nombre');
+let edad = prompt('Ingresa tu edad');
+//alerta de bienvenidad
+alert('¡Bienvenida y bienvenido a nuestro sitio web!');
+
+let saldoDisponibles=1000;
+let mensajeDeError="¡Error! Completa todos los campos"
+
+if (edad >= 18){
+    alert('¡Puedes obtener tu licencia de conducir!');
+}
+
