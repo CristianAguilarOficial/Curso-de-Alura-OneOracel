e5a7d43a2cda51483a2e4ae8622de5254e4184d0
CristianAguilarOficial <aguilarcristian060@gmail.com>
2024-06-23 12:16:27 -0500

    se agregar la carpeta introducion clase 1

---
+++ 2034-logica-programacion-1-Aula1/2034-logica-programacion-1-Aula1/app.js
@@ -0,0 +1,8 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un n�mero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}
