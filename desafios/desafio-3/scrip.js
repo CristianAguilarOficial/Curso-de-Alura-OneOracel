dda1a9f11d8e7f1921d2bb69bf11fe798a7d5a73
CristianAguilarOficial <aguilarcristian060@gmail.com>
2024-07-01 20:57:43 -0500

    ingresando archivos del desafio 3 while

---
+++ desafios/desafio-3/scrip.js
@@ -0,0 +1,22 @@
+//variables
+let numero = 1;
+let cnumero = 10;
+let cuentaRegresiva = prompt('ingresa un numero');
+let contador= 0;
+
+console.log("punto 1 ---------------------------------------------");
+while (numero <= 10) {
+    console.log("numero: " + numero);
+    numero++;
+} 
+console.log("punto 2 ---------------------------------------------");
+ while( 1 <= cnumero ){
+console.log("Contador : " + cnumero );
+cnumero--;
+ }
+
+console.log("punto 3 ---------------------------------------------");
+while (contador <= cuentaRegresiva) {
+    console.log("cuenta regresiva: " + contador);
+    contador++;
+}
