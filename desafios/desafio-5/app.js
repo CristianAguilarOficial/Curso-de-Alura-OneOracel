60d1e17d326f2cd97ed2decca96ebfedbd6463d8
CristianAguilarOficial <aguilarcristian060@gmail.com>
2024-07-01 23:27:44 -0500

    modificacion final del juego

--- desafios/desafio-5/app.js
+++ desafios/desafio-5/app.js
@@ -1,8 +1,28 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+//variables
+let numeroMaximoPosible =10;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible + 1);
+let numeroUsuario = 0;
+let intentos=1;
+let macimosIntentos=4;
 
-console.log(numeroUsuario);
+while (numeroUsuario != numeroSecreto) {
+ numeroUsuario = parseInt(prompt(`Me indica un número entre 1 y ${numeroMaximoPosible} por favor:`));
+ console.log(typeof(numeroUsuario));
+ if (numeroUsuario == numeroSecreto) {
+    alert(`acertaste, el número es: ${numeroUsuario}. lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    
+ } else {
+    if(numeroUsuario > numeroSecreto){
+        alert(`el número secreto es menor`);
+    }else{
+        alert(`El número secreto es mayor`);
+    }
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    intentos = intentos +1;
+    palabraVeces="veces";
+    if(intentos > 3){
+        alert(`Llegaste al numero maximo de intentos, el numero secreto era: ${numeroSecreto}`);
+        break;
+    }
+ }
 }
